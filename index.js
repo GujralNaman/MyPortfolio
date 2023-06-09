b3006ec603f65790f0fa7a4231d322aa5f44c202
GujralNaman <gujralnaman2002@gmail.com>
2023-06-09 19:38:12 +0530

    hero section updated

---
+++ index.js
@@ -0,0 +1,14 @@
+const text = document.querySelector(".sec-text");
+    const textLoad = () => {
+        setTimeout(() => {
+            text.textContent = "Web Developer";
+        }, 0);
+        setTimeout(() => {
+            text.textContent = "Coding Enthusiast";
+        }, 4000);
+        setTimeout(() => {
+            text.textContent = "Learner";
+        }, 8000);
+    }
+    textLoad();
+    setInterval(textLoad, 12000);
