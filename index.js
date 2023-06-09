1fd1577b685638f818a4914eb513998cb9037f34
GujralNaman <gujralnaman2002@gmail.com>
2023-06-09 20:13:42 +0530

    hero section updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const text = document.querySelector(".sec-text");
     const textLoad = () => {
         setTimeout(() => {
-            text.textContent = "Web Developer";
+            text.textContent = "Web Developer...";
         }, 0);
         setTimeout(() => {
-            text.textContent = "Coding Enthusiast";
+            text.textContent = "Coding Enthusiast...";
         }, 4000);
         setTimeout(() => {
-            text.textContent = "Learner";
+            text.textContent = "Learner....";
         }, 8000);
     }
     textLoad();
